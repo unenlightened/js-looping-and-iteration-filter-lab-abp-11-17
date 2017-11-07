3a919897918860aa466d4c726f8f1ba23e77a44c
Chely Ho <adept_inept@yahoo.com>
2017-11-07 05:44:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file
 
 function findMatching(collection, name) {
-  return collection.filter(function(name) {
-    return name;
+  return collection.filter(function(check) {
+    return check === name;
   });
-  
+
 }
