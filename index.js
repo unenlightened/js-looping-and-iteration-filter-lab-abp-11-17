05c79ce0ddb98c9d41899c2fabfdd486f2b8c4b4
Chely Ho <adept_inept@yahoo.com>
2017-11-07 05:29:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Code your solution in this file
 
 function findMatching(collection, name){
-  function checkName(name){
-    return test === name;
-  }
-  return collection.filter(checkName);
+  const matching = collection.filter(function(name){return name===name;});
+  return matching;
 }
