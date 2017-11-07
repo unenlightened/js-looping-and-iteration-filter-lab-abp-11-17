07100d579d48f67920e5df24cc9b533a7f21a987
Chely Ho <adept_inept@yahoo.com>
2017-11-07 06:54:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file
+
+function findMatching(collection, name) {
+  return collection.filter(function(check) {
+    return check.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(collection, name){
+  return collection.filter(function(check) {
+    return check.toLowerCase().indexOf(name.toLowerCase()) === 0;
+  });
+}
+
+function matchName(collection, name) {
+  return collection.filter(function(check){
+      for(const key in check) {
+        return check[key] === name;
+      }
+  });
+
+}
