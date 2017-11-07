07a6c6a8a505640edc9fa971b0f88336a86ec74a
Chely Ho <adept_inept@yahoo.com>
2017-11-07 04:52:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file
 
 function findMatching(collection, name){
-  for (const user of collection){
-    
+  collection.filter(function(name) return (name === name;));
   }
 }
