471149b862a93f18a105d8bb69d55e2f384273b1
Chely Ho <adept_inept@yahoo.com>
2017-11-07 05:16:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Code your solution in this file
+const drivers = [1, 2, 3, 4, 5];
 
-[1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
+drivers.filter(function (num) { return num > 3; });
+
+
